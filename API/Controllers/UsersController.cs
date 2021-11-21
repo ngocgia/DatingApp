215b85b60cf82fb34ab3140ea6baa6a1250f12ba
ngocgia <vodangngocgia@gmail.com>
2021-11-21 21:09:42 +0700

    error in 157

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -30,9 +31,12 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUser()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUser([FromQuery]UserParams userParams)
         {
-            var users = await _userRepository.GetMembersAsync();
+            var users = await _userRepository.GetMembersAsync(userParams);
+
+            Response.AddPaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
+
             return Ok(users);
         }
        
---
+++ API/Helpers/PaginationHeader.cs
@@ -0,0 +1,18 @@
+namespace API.Helpers
+{
+    public class PaginationHeader
+    {
+        public PaginationHeader(int currentPage, int itemPerPage, int totalItems, int totalPages)
+        {
+            CurrentPage = currentPage;
+            ItemPerPage = itemPerPage;
+            TotalItems = totalItems;
+            TotalPages = totalPages;
+        }
+
+        public int CurrentPage { get; set; }
+        public int ItemPerPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
