58bc7dddbe5beb83152871e61c27c72a83585338
Ngọc Giả <vodangngocgia@gmail.com>
2021-12-02 21:19:38 +0700

    203

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -31,12 +31,9 @@ namespace API.Controllers
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
             var user = _mapper.Map<AppUser>(registerDto);
-            using var hmac = new HMACSHA512();
            
             user.UserName = registerDto.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
-
+        
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
@@ -56,15 +53,6 @@ namespace API.Controllers
                 .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if (user == null) return Unauthorized("Invalid username");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computeHash.Length; i++)
-            {
-                if (computeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-            }
 
             return new UserDto
             {
--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -16,13 +16,10 @@ namespace API.Data
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeeData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+            if(users == null) return;
             foreach (var user in users)
             {
-                using var hmac = new HMACSHA512();
-
                 user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd")); // mật khẩu mặc định của dev
-                user.PasswordSalt = hmac.Key;
 
                 context.Users.Add(user);
             }
