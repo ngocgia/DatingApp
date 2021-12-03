4391894603ae144e4d65b29d4a8a01eb2a828e84
Ngọc Giả <vodangngocgia@gmail.com>
2021-12-03 20:44:26 +0700

    style online

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
+import { PresenceService } from './_services/presence.service';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +13,17 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService, private presence:PresenceService) {}
 
   ngOnInit() {
-    // this.getUsers();
     this.setCurrentUser();
   }
   setCurrentUser(){
     const user: User = JSON.parse(localStorage.getItem("user")!);
-    this.accountService.setCurrentUser(user);
+    if(user){
+      this.accountService.setCurrentUser(user);
+      this.presence.createHubConnection(user);
+    }
   }
 
 }
