68f22e9a7d05bce4e2672e76dfc49ad9ccda8267
ngocgia <vodangngocgia@gmail.com>
2022-03-24 09:47:42 +0700

    add

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -46,7 +46,7 @@ export class MessageService {
     })
 
     this.hubConnection.on('UpdatedGroup', (group: Group) => {
-      if (group.connections.some(x => x.username === otherUsername)) {
+      if (group.connections.some((x) => x.username === otherUsername)) {
         this.messageThread$.pipe(take(1)).subscribe(messages => {
           messages.forEach(message => {
             if (message.dateRead) {
